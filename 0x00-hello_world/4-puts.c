a0ff5b77402cc5762b537297f6b80197f128e1d8
Your Name <you@example.com>
2023-08-10 14:03:18 -0700

    k

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int(void)
+{
+puts("\"Programming is like building a multilingual puzzle)
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f:
+		print f ("size of char: %lu byte(s)\n',(unsigned long)
+				sizeof(a));
+
